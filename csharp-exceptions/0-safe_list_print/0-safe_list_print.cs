3531439e242bbe4e4ed24fdb01d8835d41df2dbf
lanaarafat <a.mutoni1@alustudent.com>
2024-06-08 10:23:45 +0200

    exception

--- csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -5,11 +5,13 @@ class List
 {
     public static int SafePrint(List<int> myList, int n)
     {
+        //List<int> new_list = new List<int>();
         int i;
         int count = 0;
+
         try
         {
-            for(int i = 0; i<n; i++)
+            for (i = 0 ; i < n ; i++)
             {
                 Console.WriteLine(myList[i]);
                 count++;
