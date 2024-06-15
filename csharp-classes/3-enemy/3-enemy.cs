669de30fe6dcbf776d679dfe8e10e38cc0758f64
lanaarafat <a.mutoni1@alustudent.com>
2024-06-15 18:20:10 +0200

    classes

---
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Enemies
+{
+    class Zombie
+    {
+        public int health;
+        public Zombie()
+        {
+            health = 0;
+        }
+        public Zombie(int value)
+        {
+            if (value >= 0)
+                this.health = value;
+            else if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+        }
+        public int GetHealth()
+        {
+            return(this.health);
+        }
+    }
+}
---
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace Enemies
+{
+    class Zombie
+    {
+        public int health;
+        public Zombie()
+        {
+            health = 0;
+        }
+        public Zombie(int value)
+        {
+            if (value >= 0)
+                this.health = value;
+            else if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+        }
+        public int GetHealth()
+        {
+            return(this.health);
+        }
+        private string name = "(No name)";
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public int GetHealth()
+        {
+            return(this.health);
+        }
+        public override string ToString()
+        {
+            return($"Zombie Name: {this.name} / Total Health: {this.health}")
+        }
+    }
+}
