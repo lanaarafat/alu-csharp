288ce1c9ef1256d3340959e852939275f4d6a7ce
lanaarafat <a.mutoni1@alustudent.com>
2024-06-15 17:51:54 +0200

    classes

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Enemies
+{
+    class Zombie
+    {
+        public int health;
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Enemies
+{
+    class Zombie
+    {
+        public int health;
+        public Zombie()
+        {
+            health = 0;
+        }
+        public Zombie(int value)
+        {
+            if (value >= 0)
+                this.health = value;
+            else if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+        }
+    }
+}
