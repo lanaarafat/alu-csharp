841fc7ab1f965a79751c6587603e4ebd185000da
lanaarafat <a.mutoni1@alustudent.com>
2024-06-27 09:53:31 +0200

    methods

--- csharp-inheritance/3-type_get/3-type_get.cs
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Reflection;
 
+/// <summary>
+/// This is our class Obj that contains methods to print properties and methods of any object.
+/// </summary>
 class Obj
 {
     public static void Print(object myObj)
     {
-        
+        Type t = myObj.GetType();
+        PropertyInfo[] props = t.GetProperties();
+
+        Console.WriteLine(t.Name + " Properties:");
+        foreach (var element in props)
+            Console.WriteLine(element.Name);
     }
+        Type t = myObj.GetType();
+        MethodInfo[] methods = t.GetMethods();
+        Console.WriteLine(t.Name + " Methods:");
+        foreach (var element in methods)
+            Console.WriteLine(element.Name);
 }
