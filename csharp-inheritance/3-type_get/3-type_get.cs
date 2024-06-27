4612240953f9277b5d51b69b2ed08542a867693a
lanaarafat <a.mutoni1@alustudent.com>
2024-06-27 09:59:21 +0200

    type info

--- csharp-inheritance/3-type_get/3-type_get.cs
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -9,15 +9,16 @@ class Obj
     public static void Print(object myObj)
     {
         Type t = myObj.GetType();
+        TypeInfo myType = t.GetTypeInfo();
         PropertyInfo[] props = t.GetProperties();
+        MethodInfo[] methods = t.GetMethods();
 
         Console.WriteLine(t.Name + " Properties:");
         foreach (var element in props)
             Console.WriteLine(element.Name);
-    }
-        Type t = myObj.GetType();
-        MethodInfo[] methods = t.GetMethods();
+            
         Console.WriteLine(t.Name + " Methods:");
         foreach (var element in methods)
             Console.WriteLine(element.Name);
+    }
 }
