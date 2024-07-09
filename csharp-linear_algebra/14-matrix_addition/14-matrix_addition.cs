0ca83c3bb3dcaae2d2c6b9544d36c0bdfacaa741
lanaarafat <a.mutoni1@alustudent.com>
2024-07-09 13:39:01 +0200

    matrix

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -4,10 +4,10 @@ class MatrixMath
 {
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        int row1 = matrix1.GetLength(0);
-        int col1 = matrix1.GetLength(1);
-        int row2 = matrix2.GetLength(0);
-        int col2 = matrix2.GetLength(1);
+        int rows1 = matrix1.GetLength(0);
+        int cols1 = matrix1.GetLength(1);
+        int rows2 = matrix2.GetLength(0);
+        int cols2 = matrix2.GetLength(1);
 
         if ((rows1 == 2 && (cols1 == 2 || cols1 == 3) && rows2 == 2 && (cols2 == 2 || cols2 == 3) && rows1 == rows2 && cols1 == cols2) ||
             (rows1 == 3 && cols1 == 3 && rows2 == 3 && cols2 == 3 && rows1 == rows2 && cols1 == cols2))
