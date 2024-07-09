e5231b18aaea21fe0ea2c6aa7bed3b0ab1fe3c68
lanaarafat <a.mutoni1@alustudent.com>
2024-07-09 13:35:44 +0200

    matrix

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -4,6 +4,24 @@ class MatrixMath
 {
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        
+        int row1 = matrix1.GetLength(0);
+        int col1 = matrix1.GetLength(1);
+        int row2 = matrix2.GetLength(0);
+        int col2 = matrix2.GetLength(1);
+
+        if ((rows1 == 2 && (cols1 == 2 || cols1 == 3) && rows2 == 2 && (cols2 == 2 || cols2 == 3) && rows1 == rows2 && cols1 == cols2) ||
+            (rows1 == 3 && cols1 == 3 && rows2 == 3 && cols2 == 3 && rows1 == rows2 && cols1 == cols2))
+        {
+            return new double[,] { { -1 }};
+        }
+
+        double[,] result = new double[rows1, cols1];
+
+        for (int j = 0; j < cols1; j++)
+        {
+            result[i, j] = matrix1[i, j] + matrix2[i, j];
+        }
+
+        return result;
     }
 }
---
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,9 @@
+using System;
+
+class MatricMath
+{
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        if (matrix1.GetLength(0) != 2 && matrix1.GetLength(1) != 3 || )
+    }
+}
