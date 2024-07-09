3ea90e741d4dcb0494139b57be05834ef83cb67c
lanaarafat <a.mutoni1@alustudent.com>
2024-07-09 13:44:04 +0200

    matrix

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -9,19 +9,27 @@ class MatrixMath
         int rows2 = matrix2.GetLength(0);
         int cols2 = matrix2.GetLength(1);
 
+        // Check if both matrices are either 2D (2x2, 2x3) or 3D (3x3) and of the same size
         if ((rows1 == 2 && (cols1 == 2 || cols1 == 3) && rows2 == 2 && (cols2 == 2 || cols2 == 3) && rows1 == rows2 && cols1 == cols2) ||
             (rows1 == 3 && cols1 == 3 && rows2 == 3 && cols2 == 3 && rows1 == rows2 && cols1 == cols2))
         {
-            return new double[,] { { -1 }};
+            // Initialize the resulting matrix
+            double[,] result = new double[rows1, cols1];
+
+            // Add corresponding elements
+            for (int i = 0; i < rows1; i++)
+            {
+                for (int j = 0; j < cols1; j++)
+                {
+                    result[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            }
+
+            return result;
         }
-
-        double[,] result = new double[rows1, cols1];
-
-        for (int j = 0; j < cols1; j++)
+        else
         {
-            result[i, j] = matrix1[i, j] + matrix2[i, j];
+            return new double[,] { { -1 } };
         }
-
-        return result;
     }
 }
