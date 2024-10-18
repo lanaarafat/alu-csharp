a8d8052280999feacf9e3c9283a4bd3c0635203f
lanaarafat <a.mutoni1@alustudent.com>
2024-10-18 17:30:44 +0200

    xml

--- csharp-generics/1-enqueue/queue.cs
+++ csharp-generics/1-enqueue/queue.cs
@@ -1,62 +1,75 @@
-using System;
-
-/// <summary>
-/// This is Queue.
+﻿/// <summary>
+/// Generic class Queue
 /// </summary>
+/// <typeparam name="T"></typeparam>
+
 public class Queue<T>
 {
     /// <summary>
-    /// This is Queue empty class.
+    /// Head of the Queue
     /// </summary>
-    public Type CheckType()
-    {
-        return(typeof(T));
-    }
-
+    public Node? head;
     /// <summary>
-    /// This is Queue empty class.
+    /// Tail of the Queue
+    /// </summary>
+    public Node? tail;
+    /// <summary>
+    /// Count of the Nodes
+    /// </summary>
+    public int count;
+    /// <summary>
+    /// The Node class
     /// </summary>
     public class Node
     {
-        /// <summary>This is Queue empty class.</summary>
-        public T value = default(T);
-        /// <summary>This is Queue empty class.</summary>
-        public Node next = null;
-
-        /// <summary>This is Queue empty class.</summary>
-        public Node(T var)
-        {
-            value = var;
+        /// <summary>
+        /// Value of a node
+        /// </summary>
+        public T? value = default;
+        /// <summary>
+        /// Next pointer of a node
+        /// </summary>
+        public Node? next = null;
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="v"></param>
+        public Node(T v) {
+            value = v;
+            next = null;
         }
+        
+    }
+    /// <summary>
+    /// Method that returns the type of the Queue
+    /// </summary>
+    /// <returns></returns>
+    public Type CheckType()
+    {
+        return typeof(T);
     }
 
-    /// <summary>This is Queue empty class.</summary>
-    public Node head = null;
-    /// <summary>This is Queue empty class.</summary>
-    public Node tail = null;
-    /// <summary>This is Queue empty class.</summary>
-    public int count;
-
-    /// <summary>This is Queue empty class.</summary>
-    public void Enqueue(T value)
-    {
-        Node newNode = new Node(value);
-        if (head == null)
-        {
+    /// <summary>
+    /// Method that adds an item to the queue
+    /// </summary>
+    /// <param name="value"></param>
+    public void Enqueue(T? value) {
+        Node newNode = new Node(value!);
+        if(head == null) {
             head = newNode;
             tail = newNode;
-        }
-        else
-        {
-            tail.next = newNode;
+        }else {
+            tail!.next = newNode;
             tail = newNode;
         }
         count++;
     }
+    /// <summary>
+    /// Method to count all the nodes of a queue
+    /// </summary>
+    /// <returns></returns>
 
-    /// <summary>This is Queue empty class.</summary>
-    public int Count()
-    {
-        return(count);
+    public int Count() {
+        return count;
     }
 }
