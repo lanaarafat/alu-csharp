a9d9d7d0b90a1ee39ee9f06fdb38203ec8ce4575
lanaarafat <a.mutoni1@alustudent.com>
2024-10-18 17:13:39 +0200

    type

--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -2,6 +2,10 @@ using System;
 
 public class Queue<T>
 {
+    /// <summary>
+    /// method type
+    /// </summary>
+    /// <returns></returns>
     public Type CheckType()
     {
         return typeof(T);
