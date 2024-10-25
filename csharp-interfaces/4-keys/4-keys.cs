72cc1cbcdeda43ccfefedfcb39d782b73cc98155
lanaarafat <a.mutoni1@alustudent.com>
2024-10-25 07:56:57 +0200

    key

--- csharp-interfaces/4-keys/4-keys.cs
+++ csharp-interfaces/4-keys/4-keys.cs
@@ -1,145 +1,190 @@
 using System;
-/// <summary>
-/// abstract class
-/// </summary>
 
+/// <summary>
+/// Base abstract class.
+/// </summary>
 public abstract class Base
 {
     /// <summary>
-    /// abstract class
+    /// Name of the object.
     /// </summary>
-    public string name;
+    public string? name { get; set; }
+
     /// <summary>
-    /// empty
+    /// Provides a string representation of the object.
     /// </summary>
-    /// <returns></returns>
-    public override string Tostring()
+    /// <returns>String in the format: "name is a [type]"</returns>
+    public override string ToString()
     {
-        /// <summary>ToString</summary>
-        return($"{name} is a {this.GetType()}");
-    }
-
-    public interface IInteractive
-    {
-        /// <summary>
-        /// IInteractive
-        /// </summary>
-        void Interact();
-    }
-
-    public interface IBreakable
-    {
-        /// <summary>
-        /// IBreakable
-        /// </summary>
-        int durability{get; set; };
-        void Break();
-
-    }
-
-    public interface ICollectable
-    {
-        /// <summary>
-        /// ICollectable
-        /// </summary>
-        bool isCollectable{get; set; };
-        void Collect();
+        return $"{name} is a {this.GetType().Name}";
     }
 }
 
+/// <summary>
+/// IInteractive interface.
+/// </summary>
+public interface IInteractive
+{
+    /// <summary>
+    /// Interact with the object.
+    /// </summary>
+    void Interact();
+}
+
+/// <summary>
+/// IBreakable interface.
+/// </summary>
+public interface IBreakable
+{
+    /// <summary>
+    /// Gets or sets the durability of the object.
+    /// </summary>
+    int durability { get; set; }
+
+    /// <summary>
+    /// Break the object.
+    /// </summary>
+    void Break();
+}
+
+/// <summary>
+/// ICollectable interface.
+/// </summary>
+public interface ICollectable
+{
+    /// <summary>
+    /// Gets or sets whether the object is collected.
+    /// </summary>
+    bool isCollected { get; set; }
+
+    /// <summary>
+    /// Collect the object.
+    /// </summary>
+    void Collect();
+}
+
+/// <summary>
+/// Represents a Door that can be interacted with.
+/// </summary>
 public class Door : Base, IInteractive
 {
     /// <summary>
-    /// class Door
+    /// Initializes a new instance of the Door class.
     /// </summary>
-    /// <param name="name"></param>
+    /// <param name="name">The name of the door.</param>
     public Door(string name = "Door")
     {
-        /// <summary>ToString</summary>
         this.name = name;
     }
+
+    /// <summary>
+    /// Interacts with the Door, attempting to open it.
+    /// </summary>
     public void Interact()
     {
-        /// <summary>ToString</summary>
         Console.WriteLine($"You try to open the {name}. It's locked.");
     }
 }
 
+/// <summary>
+/// Decoration
+/// </summary>
 public class Decoration : Base, IInteractive, IBreakable
 {
-    /// <summary>This is Interact.</summary>
+    /// <summary>
+    /// durability
+    /// </summary>
     public int durability{ get; set; }
-    /// <summary>This is Interact.</summary>
+    /// <summary>
+    /// isQuestion
+    /// </summary>
     public bool isQuestItem;
 
-    /// <summary>This is Interact.</summary>
+    /// <summary>
+    /// decoration
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="durability"></param>
+    /// <param name="isQuestItem"></param>
+    /// <exception cref="Exception"></exception>
     public Decoration(string name = "Decoration", int durability = 1, bool isQuestItem = false)
     {
-        if (durability <= 0)
-        {
-            throw new Exception("Durability must be greater than 0");
-        }
-
         this.name = name;
-        this.durability = durability;
+        if (durability <= 0) {
+            throw new Exception("Durability must be greater than 0");
+        } else {
+            this.durability = durability;
+        }
         this.isQuestItem = isQuestItem;
     }
-    public void Interact()
-    {
-        if (durablility <= 0)
-        {
-            Console.WriteLine($"The {name} has been broken.");
-        }
-        else if (isQuestion == true)
-        {
-            Console.WriteLine($"You look at the {name}. There's a key inside");
 
-        }
-        else (isQuestion == false)
-        {
-            console.WriteLine($"You look at the {name}. Not much to see here.");
+    /// <summary>
+    /// Interact implementation
+    /// </summary>
+
+    public void Interact() {
+        if (durability <= 0) {
+            Console.WriteLine($"The {name} has been broken.");
+        } else {
+            if (isQuestItem) {
+            Console.WriteLine($"You look at the {name}. There's a key inside.");
+            } 
+            else {
+            Console.WriteLine($"You look at the {name}. Not much to see here.");
+            }
         }
     }
 
-    public void Break()
-    {
+    /// <summary>
+    /// Break implementation
+    /// </summary>
+    public void Break() {
         durability--;
-        if (durablility > 0)
-        {
-            Console.WriteLine($"You hit the {name}. It cracks.")
-        }
-        else if (durability == 0)
-        {
+        if (durability > 0) {
+            Console.WriteLine($"You hit the {name}. It cracks.");
+        } else if (durability == 0) {
             Console.WriteLine($"You smash the {name}. What a mess.");
-
-        }
-        else (durability < 0)
-        {
+        } else {
             Console.WriteLine($"The {name} is already broken.");
         }
     }
+}
 
-    public class Key : Base, ICollectable
+/// <summary>
+/// key
+/// </summary>
+public class Key : Base, ICollectable
+{
+    /// <summary>
+    /// iscollected
+    /// </summary>
+    public bool isCollected{get; set;}
+
+    /// <summary>
+    /// key
+    /// </summary>
+
+    public  Key(string name = "Key", bool isCollectable = false)
     {
-        public bool isCollected{get; set;}
-        public  Key(string name = "Key", bool isCollectable = false)
+        this.name = name;
+        this isCollectable = isCollectable;
+    }
+
+    /// <summary>
+    /// collect
+    /// </summary>
+
+    public void Collect()
+    {
+        if (isCollected == false)
         {
-            this.name = name;
-            this isCollectable = isCollectable;
+            isCollected = true;
+            Console.WriteLine($"You pick the {name}.");
         }
 
-        public void Collect()
+        else (isCollected == true)
         {
-            if (isCollected == false)
-            {
-                isCollected = true;
-                Console.WriteLine($"You pick the {name}.");
-            }
-
-            else (isCollected == true)
-            {
-                Console.WriteLine($"You already picked up the {name}.");
-            }
+            Console.WriteLine($"You already picked up the {name}.");
         }
     }
 }
