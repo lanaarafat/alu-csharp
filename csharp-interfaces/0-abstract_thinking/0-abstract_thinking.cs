556c2c11e5f216bc471d642e0f2b244919799b65
lanaarafat <a.mutoni1@alustudent.com>
2024-10-25 06:44:19 +0200

    abstract

--- csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -8,7 +8,7 @@ public abstract class Base
     /// <summary>
     /// Name of the object.
     /// </summary>
-    public string name;
+    public string name {get; set;}
 
     /// <summary>
     /// Provides a string representation of the object.
@@ -16,6 +16,6 @@ public abstract class Base
     /// <returns>String in the format: "name is a [type]"</returns>
     public override string ToString()
     {
-        return $"{name} is a {this.GetType()}";
+        return $"{name} is a {this.GetType().Name}";
     }
 }
