c0d72a15916c909dfae4604b68f331031912f84e
lanaarafat <a.mutoni1@alustudent.com>
2024-10-25 07:23:11 +0200

    door

--- csharp-interfaces/2-doors/2-doors.cs
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -1,51 +1,71 @@
 using System;
-/// <summary>
-/// abstract class
-/// </summary>
 
+/// <summary>
+/// Base abstract class.
+/// </summary>
 public abstract class Base
 {
     /// <summary>
-    /// abstract class
+    /// Name of the object.
     /// </summary>
-    public string name;
+    public string? name { get; set; }
+
     /// <summary>
-    /// empty
+    /// Provides a string representation of the object.
     /// </summary>
-    /// <returns></returns>
-    public override string Tostring()
+    /// <returns>String in the format: "name is a [type]"</returns>
+    public override string ToString()
     {
-        /// <summary>ToString</summary>
-        return($"{name} is a {this.GetType()}");
-    }
-
-    public interface IInteractive
-    {
-        /// <summary>
-        /// IInteractive
-        /// </summary>
-        void Interact();
-    }
-
-    public interface IBreakable
-    {
-        /// <summary>
-        /// IBreakable
-        /// </summary>
-        int durability { get; set; }
-        void Break();
-
-    }
-
-    public interface ICollectable
-    {
-        /// <summary>
-        /// ICollectable
-        /// </summary>
-        bool isCollectable { get; set; }
-        void Collect();
+        return $"{name} is a {this.GetType().Name}";
     }
 }
+
+/// <summary>
+/// IInteractive interface.
+/// </summary>
+public interface IInteractive
+{
+    /// <summary>
+    /// Interact with the object.
+    /// </summary>
+    void Interact();
+}
+
+/// <summary>
+/// IBreakable interface.
+/// </summary>
+public interface IBreakable
+{
+    /// <summary>
+    /// Gets or sets the durability of the object.
+    /// </summary>
+    int durability { get; set; }
+
+    /// <summary>
+    /// Break the object.
+    /// </summary>
+    void Break();
+}
+
+/// <summary>
+/// ICollectable interface.
+/// </summary>
+public interface ICollectable
+{
+    /// <summary>
+    /// Gets or sets whether the object is collected.
+    /// </summary>
+    bool isCollected { get; set; }
+
+    /// <summary>
+    /// Collect the object.
+    /// </summary>
+    void Collect();
+}
+
+/// <summary>
+/// TestObject class inheriting from Base and implementing IInteractive, IBreakable, and ICollectable.
+/// </summary>
 
 public class Door : Base, IInteractive
 {
