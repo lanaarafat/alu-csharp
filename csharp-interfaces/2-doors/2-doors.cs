f491f61f7e021ff21d5256042eaeb48a30944fa9
lanaarafat <a.mutoni1@alustudent.com>
2024-10-25 07:16:35 +0200

    door

--- csharp-interfaces/2-doors/2-doors.cs
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -32,7 +32,7 @@ public abstract class Base
         /// <summary>
         /// IBreakable
         /// </summary>
-        int durability{get; set; };
+        int durability { get; set; }
         void Break();
 
     }
@@ -42,7 +42,7 @@ public abstract class Base
         /// <summary>
         /// ICollectable
         /// </summary>
-        bool isCollectable{get; set; };
+        bool isCollectable { get; set; };
         void Collect();
     }
 }
